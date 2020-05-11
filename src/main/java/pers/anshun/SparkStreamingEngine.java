49cb9c03265ad10374da7ab1defc07cdba6e519a
anshun_zhu <nupt_b09040212@sina.com>
2020-05-11 20:25:14 +0800

    [function] (receive messages from kafka)
    {}

---
+++ src/main/java/pers/anshun/SparkStreamingEngine.java
@@ -0,0 +1,58 @@
+package pers.anshun;
+
+
+import com.google.common.base.Preconditions;
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pers.anshun.receiver.KafkaReceiver;
+
+public class SparkStreamingEngine {
+    private static final Logger log = LoggerFactory.getLogger(SparkStreamingEngine.class);
+    private JavaStreamingContext jssc;
+
+    private SparkStreamingEngine(String appName) {
+        SparkConf sparkConf = new SparkConf().setAppName(appName);
+        this.jssc = new JavaStreamingContext(sparkConf, Durations.seconds(2));
+    }
+
+    private void StartSpark() {
+        KafkaReceiver kafkaReceiver = new KafkaReceiver(jssc);
+        JavaDStream<String> nginxLogs = kafkaReceiver.GetLogs();
+        nginxLogs.print();
+        jssc.start();
+    }
+
+    private void AwaitSpark() throws InterruptedException {
+        Preconditions.checkState(jssc != null);
+        log.warn("Spark Streaming is running");
+        jssc.awaitTermination();
+    }
+
+    private void StopSpark() {
+        if (jssc != null) {
+            log.warn("Shutting down Spark Streaming; this may take some time");
+            jssc.stop(true, true);
+            jssc = null;
+        } else {
+            log.warn("jssc is null");
+        }
+    }
+
+    public static void main(String[] args){
+        SparkStreamingEngine engine = new SparkStreamingEngine("click-behavior-collection");
+        try {
+            engine.StartSpark();
+            engine.AwaitSpark();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            engine.StopSpark();
+        }
+    }
+}
---
+++ src/main/java/pers/anshun/receiver/KafkaReceiver.java
@@ -0,0 +1,64 @@
+package pers.anshun.receiver;
+
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import kafka.serializer.StringDecoder;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairInputDStream;
+import org.apache.spark.streaming.kafka.KafkaUtils;
+import scala.Tuple2;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class KafkaReceiver {
+    private static final Logger logger = LoggerFactory.getLogger(KafkaReceiver.class);
+    private static String kafkaIp;
+    private static String topics;
+    private static String groupName;
+    private JavaStreamingContext jssc;
+    private Map<String, String> kafkaParams;
+    private Set<String> topicsSet;
+
+    static {
+        Properties props = new Properties();
+        InputStream is = Thread.currentThread().getContextClassLoader().
+                getResourceAsStream("spark/kafka.properties");
+        try {
+            props.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        kafkaIp = props.getProperty("kafkaIp");
+        topics = props.getProperty("topics");
+        groupName = props.getProperty("groupName");
+        //kafkaIp = "172.17.1.65";
+        //topics = "ioc";
+        //groupName = "test";
+    }
+
+    public KafkaReceiver(JavaStreamingContext jssc) {
+        this.jssc = jssc;
+        this.topicsSet = new HashSet<>(Arrays.asList(topics.split(",")));
+        this.kafkaParams = new HashMap<>();
+        kafkaParams.put("metadata.broker.list", kafkaIp);
+
+        logger.info("init kafka receiver......");
+    }
+
+    public JavaDStream<String> GetLogs(){
+        logger.info("get logs from kafka......");
+        JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(
+                jssc,
+                String.class,
+                String.class,
+                StringDecoder.class,
+                StringDecoder.class,
+                kafkaParams,
+                topicsSet
+        );
+        return messages.map(Tuple2::_2);
+    }
+}
